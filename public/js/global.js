e27bce65e8a87a7e7ac53fd1140e654f9228aa37
Emma <emmaplaysdestiny@gmail.com>
2020-04-20 21:28:48 +0100

    Service worker added

---
+++ public/js/global.js
@@ -0,0 +1,12 @@
+// Make sure service workers are supported
+if('serviceWorker' in navigator) {
+    console.log('Service Worker Supported')
+    window.addEventListener('load', () => {
+        console.log('loaded')
+        // Register the service worker
+        navigator.serviceWorker
+         .register('../sw_cached_site.js')
+         .then(reg => console.log(`Service Worker: Registered`))
+         .catch(err => console.error(`Service Worker: Error: ${err}`))
+    })
+}
